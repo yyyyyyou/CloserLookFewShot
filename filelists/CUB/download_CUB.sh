2657b3c0be7be9d45073efc42e5a4191d5d03ed9
vyou <vyou@umich.edu>
2022-04-04 23:55:59 -0400

    updated CUB download link

--- filelists/CUB/download_CUB.sh
+++ filelists/CUB/download_CUB.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-wget http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz
+#updated CUB download link due to original link being removed
+wget https://s3.amazonaws.com/fast-ai-imageclas/CUB_200_2011.tgz
 tar -zxvf CUB_200_2011.tgz
 python write_CUB_filelist.py
